108fb28ea26862d4aa8663f87a41fb0cf8524b79
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-03-30 22:38:05 +0300

    add react route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
+import CoinDetails from './components/CoinDetails/CoinDetails';
 import Coins from './components/Coins/Coins';
+import BdAddress from './components/Contact/BdAddress';
 import Contact from './components/Contact/Contact';
+import UsaAddress from './components/Contact/UsaAddress';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Homepage from './components/Homepage/Homepage';
 
@@ -13,9 +17,14 @@ function App() {
       <Routes>
         <Route path='/' element={<Homepage />}></Route>
         <Route path='/coins' element={<Coins />}></Route>
+        <Route path='/coin-details/:id' element={<CoinDetails></CoinDetails>}></Route>
         <Route path='/about' element={<About />}></Route>
-        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/contact' element={<Contact />}>
+          <Route path='bd-address' element={<BdAddress></BdAddress>}></Route>
+          <Route path='usa-address' element={<UsaAddress></UsaAddress>}></Route>
+        </Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import CoinDetails from '../CoinDetails/CoinDetails';
+import CoinCard from '../CoinCard/CoinCard';
 
 const Coins = () => {
   const [coins, setCoins] = useState([])
   useEffect(() => {
-    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false`)
+    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
       .then(res => res.json())
       .then(data => setCoins(data))
   }, [])
   return (
-    <div>
-      {coins.map(coin => <CoinDetails coin={coin}></CoinDetails>)}
+    <div className='px-4 pt-10 pb-24 mx-auto max-w-7xl md:px-2'>
+      <p className='text-center text-3xl font-bold text-gray'>Available Crypto Currencies: {coins.length}</p>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-4 mt-8'>
+        {coins.map(coin => <CoinCard key={coin.id} coin={coin}></CoinCard>)}
+      </div>
     </div>
   );
 };
